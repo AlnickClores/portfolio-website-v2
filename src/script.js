fad2fbc0f48c7d064ffa5feca931865a2c378deb
Alnick Clores <aelnickclores@gmail.com>
2023-08-19 21:45:42 +0800

    centered the nav menu in mobile view

--- src/script.js
+++ src/script.js
@@ -4,17 +4,18 @@ const barsIcon = toggleButton.querySelector(".fa-bars");
 const xIcon = toggleButton.querySelector(".fa-x");
 
 toggleButton.addEventListener("click", function () {
-  mobileMenu.classList.toggle("hidden");
+  mobileMenu.classList.toggle("-left-full");
   barsIcon.classList.toggle("hidden");
   xIcon.classList.toggle("hidden");
 });
 
 function closeNavbar() {
-  mobileMenu.classList.add("hidden");
+  mobileMenu.classList.add("left-0");
   barsIcon.classList.remove("hidden");
   xIcon.classList.add("hidden");
 }
 
+//to close navbar when a link is clicked
 document.querySelectorAll(".navbar .nav-link").forEach(function (link) {
   link.addEventListener("click", closeNavbar);
 });
